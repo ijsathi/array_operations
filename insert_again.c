fe10ed37109572639c9d853139354fc68df33b48
unknown <ijsathi2001rs@gmail.com>
2023-10-15 11:43:09 +0600

    insert again

---
+++ insert_again.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int arr[n+1];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int pos, val;
+    scanf("%d%d", &pos, &val);
+    for(i=n; i>=pos+1; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[pos] = val;
+    for(i=0; i<=n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
